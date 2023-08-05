5f87a5574d0b3cf759a9f332155d7fa392154478
alireza mohammadi <alireza.mohammadi.1380.mo@gmail.com>
2023-08-05 17:36:51 +0330

    import navbar

--- src/componets/Header/header.jsx
+++ src/componets/Header/header.jsx
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from "react";
-//////////////////////////////////////////
+///////////////////////////////////////////
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -7,7 +7,19 @@ import Modal from '@mui/material/Modal';
 ///////////////////////////////////////////
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import store, {AppStateContext} from "../store/store";
-
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+//////////////////////////////////////////
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 const style = {
     position: 'absolute',
     top: '50%',
@@ -19,11 +31,7 @@ const style = {
     boxShadow: 24,
     p: 4,
 };
-const styletest = {
-    width: "100%",
-    height: "20px",
 
-}
 
 function Header() {
 
@@ -32,20 +40,161 @@ function Header() {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
     //////////////////////////////////////////////
-    // const [item,setItem]=useState();
+    /////////////////////////////////////////////
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+    ////////////////////////////////////////////
+
     let {items} = useContext(AppStateContext)
-    // console.log('teedade mahsolat ::::',items.length)
-    // let result = hook;
-    // const clickHandler = () =>{
-    //     hook.setAppState({cartOpen:true})
-    // }
-    ///////////////////////////////////////////////
-    // const {items}=useContext(store);
-    // console.log('data :', useContext(AppStateContext));
-    // console.log(typeof hook)
+
     return (
+
         <header>
-            <div className={'logo'}>
+            <div className={''}>
+                <AppBar className={'test'} position="static"  sx={{ bgcolor: 'text.secondary',m:0 }}    >
+                    <Container maxWidth="xl" sx={{m:0}} >
+                        {/*sx={{ height: '80px' }}*/}
+                        <Toolbar disableGutters sx={{m:0}}>
+                            <AdbIcon sx={
+                                {display: {xs: 'none', md: 'flex'},
+                                mr: 0,}}/>
+                            <Typography
+                                variant="h6"
+                                noWrap
+                                component="a"
+                                href="/"
+                                sx={{
+                                    mr: 2,
+                                    display: {xs: 'none', md: 'flex'},
+                                    fontFamily: 'monospace',
+                                    fontWeight: 700,
+                                    letterSpacing: '.3rem',
+                                    color: 'inherit',
+                                    textDecoration: 'none',
+                                }}
+                            >
+                                LOGO
+                            </Typography>
+
+                            <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none',}}}>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleOpenNavMenu}
+                                    color="inherit"
+                                >
+                                    <MenuIcon/>
+                                </IconButton>
+                                <Menu
+
+                                    id="menu-appbar"
+                                    anchorEl={anchorElNav}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'left',
+                                    }}
+                                    open={Boolean(anchorElNav)}
+                                    onClose={handleCloseNavMenu}
+                                    sx={{
+                                        display: {xs: 'block', md: 'none'},
+                                    }}
+                                >
+                                    {pages.map((page) => (
+                                        <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                            <Typography textAlign="center">{page}</Typography>
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Box>
+
+                            <AdbIcon sx={{display: {xs: 'flex', md: 'none'}, mr: 1,}}/>
+                            <Typography
+                                variant="h5"
+                                noWrap
+                                component="a"
+                                href=""
+                                sx={{
+                                    mr: 2,
+                                    display: {xs: 'flex', md: 'none'},
+                                    flexGrow: 1,
+                                    fontFamily: 'monospace',
+                                    fontWeight: 700,
+                                    letterSpacing: '.3rem',
+                                    color: 'inherit',
+                                    textDecoration: 'none',
+                                }}
+                            >
+                                LOGO
+                            </Typography>
+                            <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'} ,m:0}}>
+                                {pages.map((page) => (
+                                    <Button
+                                        key={page}
+                                        onClick={handleCloseNavMenu}
+                                        sx={{my: 2, color: 'white', display: 'block'}}
+                                    >
+                                        {page}
+                                    </Button>
+                                ))}
+                            </Box>
+
+                            <Box sx={{flexGrow: 0}}>
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"/>
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{mt: '45px'}}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    {settings.map((setting) => (
+                                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                            <Typography textAlign="center">{setting}</Typography>
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Box>
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+            </div>
+            <div className={'caal'}>
+                <div className={'logo'}>
                 <ShoppingCartOutlinedIcon onClick={handleOpen} className={"logo-img"}/>
                 <span>{items.length}</span>
 
@@ -69,7 +218,8 @@ function Header() {
                     </Box>
                 </Modal>
             </div>
-            <div>shoping</div>
+            <div>basket</div>
+            </div>
         </header>
     )
 
